79c21d74ab6b1ab13140ba72b590c7b9b6e733ab
Ujjwal Bodkhe <94472117+Ujjwal-Bodkhe@users.noreply.github.com>
2023-11-05 00:17:00 +0530

    Update Img.jsx

--- src/contents/Img.jsx
+++ src/contents/Img.jsx
@@ -2,6 +2,6 @@ import React from "react"
 export default function Img(){
     return (
         <div className="Img">
-             <img src="src\assets\Ujjwal Photo.jpg" alt="Ujjwal" className="Ujjwal"/>
+             <img src="https://drive.google.com/file/d/1Ad48Iv-4x2Ak4MRFPCMppK1HQOEUiQlX/view?usp=drive_link" alt="Ujjwal" className="Ujjwal"/>
         </div>
     )}
